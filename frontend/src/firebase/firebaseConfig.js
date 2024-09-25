02ba00a81fa8c18e99d6531c06226f78dcb11eaf
Pasan Chamikara <pasanchamikara989@gmail.com>
2024-09-25 19:51:01 +0530

    firebase fixed

--- frontend/src/firebase/firebaseConfig.js
+++ frontend/src/firebase/firebaseConfig.js
@@ -7,17 +7,16 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA388h-6j6bKj4TpJMZqYy6rDuCWgsLVGs",
-  authDomain: "natura-e7e71.firebaseapp.com",
-  projectId: "natura-e7e71",
-  storageBucket: "natura-e7e71.appspot.com",
-  messagingSenderId: "81336958322",
-  appId: "1:81336958322:web:50a9c3798839dc5e12f1eb",
-  measurementId: "G-QPJE52JN7Z"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 export default storage;
-
